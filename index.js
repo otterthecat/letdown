03d035d0ea7781e43958aaa1a59e298f5ac8268a
Otter <otc@otterthecat.com>
2015-10-06 23:03:02 -0500

    early support for taking generated json file and automatically inserting it into local mongodb

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 var fs = require('fs'),
+    cp = require('child_process'),
     helper = require('./lib/helper'),
-    filePath = process.env.MD_POSTS_DIR;
+    filePath = process.env.MD_POSTS_DIR,
+    cmdString = 'mongoimport --host localhost --db foobar --collection posts < ' + filePath + '/db/mongo_insertion.json --jsonArray';
 
 fs.readdir(filePath, function(err, files){
   if(err){
@@ -13,10 +15,16 @@ fs.readdir(filePath, function(err, files){
   files.forEach(function(file){
     var contents = fs.readFile(filePath + file, 'utf8', function(err, data){
       if(err){
+        // check if directory (no recursion)
+        if(err.code === 'EISDIR'){
+          console.log('file is a directory. Skipping it.');
+          return false
+        }
         console.log("Error: ", err);
         return false;
       }
-      var newPath = filePath + file.replace('.md', '.json');
+
+      var newPath = filePath + '/db/mongo_insertion.json';
       fs.writeFile(newPath, helper.createJSON(filePath + file, data), function(err){
         if(err){
           console.log("ERROR: ", err);
@@ -24,6 +32,14 @@ fs.readdir(filePath, function(err, files){
         }
 
         console.log('file created');
+        console.log('inserting into mongo...');
+        cp.exec(cmdString, function(err){
+          if(err){
+            console.log("Error inserting into DB: ", err);
+            return false;
+          }
+          console.log('insertion complete');
+        });
       });
     });
   });
