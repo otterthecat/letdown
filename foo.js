6d57d49b2562b80dbab4195c0f2bde47d2141bd6
Otter <otc@otterthecat.com>
2015-12-31 19:36:19 -0600

    remove insertion & archiving functions. Focus on creating JSON file

---
+++ foo.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log(process.env.MD_POSTS_DIR);
--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 let args = require('minimist')(process.argv.slice(2));
 let fs = require('fs');
 let cp = require('child_process');
-let query = require('./lib/query');
 let markdownToJSON = require('./lib/markdownToJSON');
 let createDetails = require('./lib/createDetails');
 let errorHandler = require('./lib/errorhandler');
@@ -18,33 +17,6 @@ let completePromise = function (resolve, reject, obj) {
       resolve(obj);
     }
   }
-};
-
-let removeFileType = function (type) {
-  return function (obj) {
-    return new Promise(function (resolve, reject) {
-      fs.unlink(obj[`${type}Path`], completePromise(resolve, reject, obj));
-    });
-  };
-};
-
-let moveToArchive = function (obj) {
-  return new Promise(function (resolve, reject) {
-    var read = fs.createReadStream(obj.mdPath);
-    var stamp = new Date().toISOString();
-    var write = fs.createWriteStream(`${obj.archivePath}${obj.fileName}_${stamp}.md`);
-    read.on('error', reject);
-    write.on('error', reject).on('finish', function () {
-      resolve(obj);
-    });
-    read.pipe(write);
-  });
-};
-
-let importToDb = function (obj) {
-  return new Promise(function (resolve, reject) {
-    cp.exec(query(obj.jsonPath, args), completePromise(resolve, reject, obj));
-  });
 };
 
 let writeDbFile = function (obj) {
@@ -72,10 +44,6 @@ fs.readdir(createDetails().rootPath, function (err, files) {
       }
 
       writeDbFile(createDetails(file, data), errorHandler)
-        .then(importToDb, errorHandler)
-        .then(moveToArchive, errorHandler)
-        .then(removeFileType('json'), errorHandler)
-        .then(removeFileType('md'), errorHandler)
         .then(function () {
           console.log('process complete');
         }, errorHandler);
